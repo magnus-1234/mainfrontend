986fdaf09b3afcca65c6f2e1dc9960539c9a6212
storage2mohitraj-ui <storage2mohitraj@gmail.com>
2026-07-03 00:04:56 +0530

    Update Music Bot client ID to 1509574640853585990 for invite link

--- src/app/HomeApp.tsx
+++ src/app/HomeApp.tsx
@@ -8944,7 +8944,7 @@ export function HomeApp({ initialMenu = "home" }: { initialMenu?: ActiveMenu } =
                     )}
                     <a 
                       className="music-btn-secondary" 
-                      href="https://discord.com/oauth2/authorize?client_id=1399025185046134866&permissions=8&integration_type=0&scope=bot" 
+                      href="https://discord.com/oauth2/authorize?client_id=1509574640853585990&permissions=8&integration_type=0&scope=bot" 
                       target="_blank" 
                       rel="noreferrer"
                     >
